e725fc644d4ebe6223800cf84569bec92bc0a609
Mike Schutte <tmikeschutte@gmail.com>
2018-10-23 08:04:17 -0400

    Update for finance lab

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const R = require("ramda");
-const { resolveSlots } = require("./lib/slot_resolver");
+const conversationResolver = require("./lib/finance");
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 
-app.post("/clinc-bizlo", (req, res) => {
-  R.pipe(
-    R.prop("body"),
-    resolveSlots,
-    JSON.stringify,
-    res.send.bind(res)
-  )(req);
+app.post("/api/v1/clinc", (req, res) => {
+  const resolved = conversationResolver(req.body);
+  console.log(JSON.stringify(req.body));
+
+  if (resolved) {
+    console.log("RESPONSE: ", JSON.stringify({ ...req.body, ...resolved }));
+    res.send(JSON.stringify({ ...req.body, ...resolved }));
+  } else {
+    // Clinc ignores any 400-500 responses
+    res.sendStatus(500);
+  }
 });
 
 module.exports = {
