58dfba660270c1d5b55851efb5de9b8e2b429ef2
Mike Schutte <schutte@clinc.com>
2018-12-04 18:22:13 -0500

    Add sandbox

--- server.js
+++ server.js
@@ -3,26 +3,43 @@ const bodyParser = require("body-parser");
 const R = require("ramda");
 const conversationResolver = require("./lib/finance");
 // const conversationResolver = require("./lib/send_money");
+const sandboxResolver = require("./lib/sandbox");
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
+const log = (label, data) => {
+  console.log(
+    `XXXXXXXXXXXXXXXXX ${label.toUpperCase()} DATA XXXXXXXXXXXXXXXXXXXX`,
+    JSON.stringify(data)
+  );
+};
 
 app.post("/api/v1/clinc", (req, res) => {
-  console.log(
-    "XXXXXXXXXXXXXXXXX REQUEST DATA XXXXXXXXXXXXXXXXXXXX",
-    JSON.stringify(req.body)
-  );
+  log("request", req.body);
 
   const resolved = conversationResolver(req.body);
 
   if (resolved) {
-    console.log(
-      "XXXXXXXXXXXXXXXXX RESPONSE DATA XXXXXXXXXXXXXXXXXXXX",
-      JSON.stringify({ ...req.body, ...resolved })
-    );
-    res.send(JSON.stringify({ ...req.body, ...resolved }));
+    const response = { ...req.body, ...resolved };
+    log("response", response);
+    res.send(JSON.stringify(response));
+  } else {
+    // Clinc ignores any 400-500 responses
+    res.sendStatus(500);
+  }
+});
+
+app.post("/api/v2/clinc", (req, res) => {
+  log("request", req.body);
+
+  const resolved = sandboxResolver(req.body);
+
+  if (resolved) {
+    const response = { ...req.body, ...resolved };
+    log("response", response);
+    res.send(JSON.stringify(response));
   } else {
     // Clinc ignores any 400-500 responses
     res.sendStatus(500);
